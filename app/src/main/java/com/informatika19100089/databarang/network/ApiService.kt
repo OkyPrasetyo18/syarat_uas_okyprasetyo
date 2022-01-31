dc6aeaee281fab6c1571a2e6cd9d4303a571d554
OkyPrasetyo18 <okirasetyo@gmail.com>
2022-01-31 21:49:32 +0700

    syarat_uas_okyprasetyo

---
+++ app/src/main/java/com/informatika19100089/databarang/network/ApiService.kt
@@ -0,0 +1,53 @@
+package com.informatika19100089.databarang.network
+
+import com.informatika19100089.databarang.model.ResponseActionBarang
+import com.informatika19100089.databarang.model.ResponseAdmin
+import com.informatika19100089.databarang.model.ResponseBarang
+import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.GET
+import retrofit2.http.POST
+
+
+interface ApiService {
+    @GET("read.php")
+    fun getBarang(): Call<ResponseBarang>
+
+    @FormUrlEncoded
+    @POST("create.php")
+    fun insertBarang(
+        @Field("nama_barang") namaBarang: String?,
+        @Field("jumlah_barang") jmlBarang: String?
+    ): Call<ResponseActionBarang>
+
+    @FormUrlEncoded
+    @POST("update.php")
+    fun updateBarang(
+        @Field("id") id: String?,
+        @Field("nama_barang") namaBarang: String?,
+        @Field("jumlah_barang") jmlBarang: String?
+    ): Call<ResponseActionBarang>
+
+    @FormUrlEncoded
+    @POST("delete.php")
+    fun deleteBarang(
+        @Field("id") id: String?
+    ): Call<ResponseActionBarang>
+
+    @FormUrlEncoded
+    @POST("login.php")
+    fun login(
+        @Field("username") username: String?,
+        @Field("password") password: String?
+    ): Call<ResponseAdmin>
+
+    @FormUrlEncoded
+    @POST("register.php")
+    fun register(
+        @Field("username") username: String?,
+        @Field("password") password: String?
+    ): Call<ResponseAdmin>
+
+
+}
